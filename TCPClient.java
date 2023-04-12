83804d7851847bcb22fedba74f8fb3dda5ad1765
ajcemca <ajcemca@mca.com>
2023-04-12 12:13:23 +0530

    Commit

---
+++ TCPClient.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClient
+{
+	public static void main(String args[]) throws IOException
+	{
+		try
+		{
+			Socket sk = new Socket("LocalHost",5555);
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String line;
+			do
+			{
+				line = br.readLine();
+				PrintWriter pw = new PrintWriter(sk.getOutputStream(),true);
+				pw.println(line);
+			}while(!line.equals("exit"));
+			sk.close();
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Cannot connect to server!!");
+		}
+	}
+}
